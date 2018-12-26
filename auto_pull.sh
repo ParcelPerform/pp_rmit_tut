1a45e13b0eac9e51218b79b065e1605f6411aba5
Kien <kien@parcelperform.com>
2018-12-26 18:28:18 +0700

    add warehouse API

---
+++ auto_pull.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## script auto pull git con subdirectory
+git clone https://github.com/ParcelPerform/shopping_cart.git # or git@github.com:ParcelPerform/shopping_cart.git if you have SSH key
+
+git clone https://github.com/ParcelPerform/warehouse_api.git # or git@github.com:ParcelPerform/warehouse_api.git if you have SSH key
+
+docker-compose up -d
