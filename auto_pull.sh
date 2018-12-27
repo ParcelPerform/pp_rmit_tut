b3ddf87ccae800155d787bcb5c8aec0b8d0a14de
Tri Truong <tri.itvn@gmail.com>
2018-12-27 09:14:48 +0700

    Update

--- auto_pull.sh
+++ auto_pull.sh
@@ -5,4 +5,6 @@ git clone https://github.com/ParcelPerform/shopping_cart.git # or git@github.com
 
 git clone https://github.com/ParcelPerform/warehouse_api.git # or git@github.com:ParcelPerform/warehouse_api.git if you have SSH key
 
+git clone https://github.com/ParcelPerform/shopping_cart_fe.git
+
 docker-compose up -d
