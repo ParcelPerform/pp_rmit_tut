547f7f5b840b268ce8c3370ff264063f8135b802
Kien <kien@parcelperform.com>
2018-12-27 10:27:31 +0700

    update docker images

--- auto_pull.sh
+++ auto_pull.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ## script auto pull git con subdirectory
+docker pull parcelperform/shopping_cart
 git clone https://github.com/ParcelPerform/shopping_cart.git # or git@github.com:ParcelPerform/shopping_cart.git if you have SSH key
 
+docker pull parcelperform/warehouse_api
 git clone https://github.com/ParcelPerform/warehouse_api.git # or git@github.com:ParcelPerform/warehouse_api.git if you have SSH key
 
 git clone https://github.com/ParcelPerform/shopping_cart_fe.git
